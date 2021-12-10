6b62903e0cb2a76fd0b6d6bd8f099a8b2034b15f
brentscott93 <43178351+brentscott93@users.noreply.github.com>
2021-12-10 14:24:24 -0500

    switch to rhandsometable

--- app.R
+++ app.R
@@ -1,103 +1,16 @@
-library(shiny)
-library(tidyverse)
-library(cowplot)
-
-hms_to_seconds <- function(hms){
-    #hms <- c("5:12", "04:22", "03:01:01")
-    hms <- str_split(hms, ":")
-    map_dbl(hms, 
-        ~if(length(.x) == 2){
-            (as.numeric(pluck(.x, 1))*60) + (as.numeric(pluck(.x, 2)))
-        } else if(length(.x) == 3){
-            (as.numeric(pluck(.x, 1))*3600) + (as.numeric(pluck(.x, 2))*60) + (as.numeric(pluck(.x, 3)))
-        }
-    )
-
-}
-
-check_time <- function(string){
-  if(nchar(string) <= 3){
-    showNotification("All times must be at least 3 digits and include a colon. Ex. 5:20", type = "error")
-    req(nchar(string)>3)
-  }
-  
-  if(nchar(string)<=5){
-    if(str_sub(string, -3, -3) != ":"){
-      showNotification("All times must be in HH:MM:SS or HH:SS", type = "error")
-      req(str_sub(string, -3, -3) == ":")
-    }
-    
-  } else if(nchar(string)<=8 & nchar(string) > 5){
-    if(str_sub(string, -3, -3) != ":"){
-      showNotification("All times must be in HH:MM:SS or HH:SS 2", type = "error")
-      req(str_sub(string, -3, -3) == ":")
-    } 
-    
-    if(str_sub(string, -6, -6) != ":"){
-      showNotification("All times must be in HH:MM:SS or HH:SS 3", type = "error")
-      req(str_sub(string, -6, -6) == ":")
-    }
-  } else if(nchar(string)>8){
-    showNotification("Stime string is too long. Must be HH:MM:SS maximum.", type = "error")
-    req(nchar(string)<=8)
-  }
-  
-  
-  if(str_detect(string, "[:alpha:]")){
-    showNotification("No letters allowed in time input.", type = "error")
-    req(!str_detect(string, "[:alpha:]"))
-    
-  }
-  
-  if(str_detect(string, "[!@#$%^&*()-+={};<>,.?\"\'_]")){
-    showNotification("Only special character allowed in time input is a ':'", type = "error")
-    req(!str_detect(string, "[!@#$%^&*()-+={};<>,.?\"\'_]"))
-  } 
-  
-}
-
-predict_time_seconds <- function(meters, d_prime, cs){
-  (meters - d_prime)/cs
-}
 
 ui <- fluidPage(
     titlePanel("Critical Speed App"),
     sidebarLayout(
-        sidebarPanel(
+        sidebarPanel(width = 5,
           tabsetPanel(id = "sidebar",
-            tabPanel("Input Data",
-                     br(),
-            sliderInput("number_of_prs", 
-                        "Select number of PRs to input:",
-                        min = 2,
-                        max = 10,
-                        value = 2),
-            uiOutput("pr_inputs"),
-            actionButton("enter", "Go")
-          ),
-          tabPanel("Sim Data",
-                   br(),
-            fluidRow(
-              column(6, 
-                     sliderInput("sim_cs", 
-                                 "Critical Speed (m/s)",
-                                 value = 4,
-                                 min = 0, 
-                                 max = 10,
-                                 step = 0.1)),
-              column(6,
-                     sliderInput("sim_d_prime", 
-                                 "D' (m)",
-                                 value = 200,
-                                 min = 0, 
-                                 max = 1000)
-                     )
-                   ),
-            actionButton("sim", "Go"),
-            )
-         )
+            tabPanel("Runner Data",
+                     rHandsontableOutput("runner_data_input")),
+            tabPanel("Race Data",
+                     rHandsontableOutput("race_data_input"))
+          )
        ),
-        mainPanel(
+        mainPanel(width = 7, 
               tabsetPanel(
                   tabPanel("Plot",
               plotOutput("lm_scatter_plot")
@@ -113,30 +26,66 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
+  output$runner_data_input <- renderRHandsontable({
+     tibble("Athlete" = rep("Athlete 1", 4),
+                "Distance (m)" = as.integer(c(1500, 3000, 5000, 10000)) ,
+                "Hours" = rep(0, 4),
+                "Minutes" = rep(0, 4),
+                "Seconds" = rep(0, 4)) %>% 
+     rhandsontable() %>% 
+     hot_col(col = "Hours", type = "dropdown", source = 0:9) %>% 
+     hot_col(col = "Minutes", type = "dropdown", source = 0:59) %>% 
+     hot_col(col = "Seconds", type = "dropdown", source = 0:59)
+  })
+  
+  
+  output$race_data_input <- renderRHandsontable({
+    tibble("Meter Marker" =  as.integer(c(200, 600, 1000)),
+           "Lap Speed (m/s)" = rep(NA, 3)) %>% 
+      rhandsontable() %>% 
+      hot_col(col = "Lap Speed (m/s)", type = "numeric") 
+    })
+  
+  
+  
+  
+ athlete_names <- reactive( paste0("athlete", seq_len(input$number_of_athletes)) )
+  
  distance_names <- reactive( paste0("distance", seq_len(input$number_of_prs)) )
     
  time_names <- reactive( paste0("time", seq_len(input$number_of_prs)) )
 
  output$pr_inputs <- renderUI({
-     l <- list(d = distance_names(), t = time_names(), n = seq_len(input$number_of_prs))
+     l <- list(a = athlete_names(),
+               d = distance_names(), 
+               t = time_names(), 
+               n = seq_len(input$number_of_prs))
      pmap(l,
-          function(d, t, n)
-            fluidRow(
+          function(a, d, t, n){
+            tabPanel(title = paste0("Athlete ", n),
+             fluidRow(
+               sliderInput(paste0(a, "number_of_prs"), 
+                           "Number of PRs",
+                           min = 2,
+                           max = 10,
+                           value = 2),
               column(
                width = 6,
                textInput(
-                 d, 
+                 paste0(a, "_", d), 
                  paste0("Distance ", n ),
                  placeholder = "meters")
                ),
              column(width = 6,
                 textInput(
-                t, 
+                paste0(a, "_", t), 
                 paste0("Time ", n),
                 placeholder = "HH:MM:SS")
            )
           )
-        )
+         )
+        }
+       )
  })
      
      user_data <- eventReactive(input$enter, {
